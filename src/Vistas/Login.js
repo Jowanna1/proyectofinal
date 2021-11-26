601618b7754f5ca13ff002e05323a34419414fa7
unknown <galvez1022@hotmail.com>
2021-11-26 10:31:17 -0500

    Login - Noticias

--- src/Vistas/Login.js
+++ src/Vistas/Login.js
@@ -4,6 +4,19 @@ export default class Login extends Component {
 
   render() {
     return (
+      <div>
+  <section class="bienvenido">
+    <form class="formulario">
+      <div class="form">
+        <input type="email" class="form-control input-config" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Ingresar usuario" v-model="usuario" />
+        <input type="password" class="form-control input-config" id="exampleInputPassword1" placeholder="Ingresar contraseña" v-model="password" />
+      </div>
+      <button type="submit" class="boton" >Ingresar</button>
+      <button type="submit" class="boton" >Crear usuario</button>
+    </form>
+  </section>
+</div>
+/*
       <div className="logueo">
         <header className="login">
           <form className="form-container">
@@ -24,6 +37,7 @@ export default class Login extends Component {
           </form>
         </header>
       </div>
+*/
     );
   }
 }
--- src/Vistas/Noticias.js
+++ src/Vistas/Noticias.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react'
 
+import ajiaco from "../assets/img/noticias/ajiaco.JPG"
+import restaurant from "../assets/img/noticias/restaurantTour.JPG"
+
 export default class Noticias extends Component {
     render() {
         return (
@@ -8,10 +11,10 @@ export default class Noticias extends Component {
                 <div class="container">
                     <div class="row">
                         <div class="col-md-6">
-                            <a href="https://www.larepublica.co/cocina"><img src="../assets/img/cocina.jpg" alt="Cocina1" class="img-thumbnail"/></a>
+                            <a href="https://www.larepublica.co/cocina"><img src={ajiaco} alt="Cocina1" class="img-thumbnail"/></a>
                         </div>
                         <div class="col-md-6">
-                            <a href="https://www.eltiempo.com/noticias/cocina-colombiana"><img src="../assets/img/cocina2.jpg" alt="Cocina2" class="img-thumbnail"/></a>
+                            <a href="https://www.eltiempo.com/noticias/cocina-colombiana"><img src={restaurant} alt="Cocina2" class="img-thumbnail"/></a>
                         </div>
                     </div>
                 </div>
